f285e966abfb52e6a049973002d988761c8d9628
holyzheng <holzpps@163.com>
2018-07-19 00:45:39 +0800

    Update bubbleSort.js

--- Algorithm/bubbleSort.js
+++ Algorithm/bubbleSort.js
@@ -21,3 +21,21 @@ function bubbleSort (arr) {
 var arr = [4,5,2,9,1,5,3,10];
 var arrTemp = bubbleSort(arr);
 console.log(arrTemp);
+
+// 二分查找
+function binarySearch(target,arr) {
+    var start   = 0;
+    var end     = arr.length-1;
+
+    while (start<=end){
+        var mid = parseInt(start+(end-start)/2);
+        if(target==arr[mid]){
+            return mid;
+        }else if(target>arr[mid]){
+            start   = mid+1;
+        }else{
+            end     = mid-1;
+        }
+    }
+    return -1;
+}
